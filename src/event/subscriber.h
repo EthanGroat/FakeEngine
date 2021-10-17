983e84b038fd121e8d853ee3242f837073c47ab7
Ethan Groat <ethantgroat@gmail.com>
2021-10-16 17:19:42 -0700

    Universe sketched out

--- src/event/subscriber.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-class ISubscriber {
-    public:
-        virtual ~ISubscriber(){};
-        virtual void receive_event(Event e) = 0;
-};
--- src/universe/universe.h
+++ src/universe/universe.h
@@ -1,18 +1,11 @@
 #pragma once
 
 #include "types.h"
-#include <entt.hpp>
+#include "entt/entt.hpp"
 
 
 namespace FakeEngine
 {
-    typedef struct entity entity;
-    struct entity
-    {
-        u_int32_t id;
-    };
-
-
     /** @brief sigleton Universe, unless and until I make a multiverse.
      * This holds references to the scenes and all entities and components 
      * in them
@@ -33,4 +26,33 @@ namespace FakeEngine
         }
     };
     
+    /*
+    possible usage ideas:
+    my_scene = Universe.get_instance().create_scene();
+    my_scene.add_entity(Entity("Box", transform_2d));
+
+    box_ent = my_scene.get_entity("Box");
+    box_ent.set_parent(other_ent);
+    box_ent.transform_add(delta_position);  // uses transform component
+
+    box_ent.add_component(
+            Universe.new_component<SoundEffect>(
+                "media/boxsounds.wav", 
+                plays_by_default=false
+                )
+        )
+    */
+
+
+    class Entity
+    {
+    private:
+        entt::entity m_entity_handle;
+        Universe* m_the_universe;
+    public:
+        Entity()
+        {
+            m_the_universe = Universe::get_instance();
+        }
+    };
 }
